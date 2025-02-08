4f309338fbc8b25ddad281dd236c2340da47c46f
lighting9999 <120090117+lighting9999@users.noreply.github.com>
2025-02-08 10:57:03 +0800

    Delete app/main.py

--- app/main.py
+++
@@ -1,9 +0,0 @@
-from fastapi import FastAPI
-from app.api import router
-
-app = FastAPI(title="Port and Ping Scanner")
-app.include_router(router)
-
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=5000)
